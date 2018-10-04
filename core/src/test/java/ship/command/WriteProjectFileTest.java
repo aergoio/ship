1d64305a5b2b72ab99f7d8ecc100bc3d9525e608
bylee <bylee78@gmail.com>
2018-10-04 11:02:15 +0900

    Enhance test case for WriteProjectFile

--- core/src/test/java/ship/command/WriteProjectFileTest.java
+++ core/src/test/java/ship/command/WriteProjectFileTest.java
@@ -7,6 +7,9 @@ package ship.command;
 import static java.util.Collections.singletonList;
 import static java.util.UUID.randomUUID;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
@@ -15,6 +18,7 @@ import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.Test;
+import org.mockito.internal.verification.AtMost;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import ship.AbstractTestCase;
 
@@ -25,7 +29,7 @@ public class WriteProjectFileTest extends AbstractTestCase {
   public void testExecute() throws Exception {
     // Given
     mockStatic(Files.class);
-    final BufferedWriter bufferedWriter = new BufferedWriter(new StringWriter());
+    final BufferedWriter bufferedWriter = mock(BufferedWriter.class);
     when(Files.newBufferedWriter(any(Path.class))).thenReturn(bufferedWriter);
 
     // When
@@ -34,6 +38,24 @@ public class WriteProjectFileTest extends AbstractTestCase {
     command.execute();
 
     // Then
+    verify(bufferedWriter).write(any(char[].class), anyInt(), anyInt());
   }
 
+  @Test
+  @PrepareForTest(WriteProjectFile.class)
+  public void shouldNotCreateFile() throws Exception {
+    // Given
+    mockStatic(Files.class);
+    when(Files.exists(any())).thenReturn(true);
+    final BufferedWriter bufferedWriter = mock(BufferedWriter.class);
+    when(Files.newBufferedWriter(any(Path.class))).thenReturn(bufferedWriter);
+
+    // When
+    final WriteProjectFile command = new WriteProjectFile();
+    command.setArguments(singletonList(randomUUID().toString()));
+    command.execute();
+
+    // Then
+    verify(bufferedWriter, new AtMost(0)).write(any(char[].class), anyInt(), anyInt());
+  }
 }
