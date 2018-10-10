839ec475ad2f0dc2212561814dc86f344701febd
bylee <bylee78@gmail.com>
2018-10-10 13:24:49 +0900

    Make ShipConstance as abstract

--- core/src/main/java/ship/ShipConstants.java
+++ core/src/main/java/ship/ShipConstants.java
@@ -4,7 +4,7 @@
 
 package ship;
 
-public class ShipConstants {
+abstract public class ShipConstants {
   public static final String PROJECT_FILENAME = "aergo.json";
 
   public static final String MODULES_BASE = ".aergo_modules";
