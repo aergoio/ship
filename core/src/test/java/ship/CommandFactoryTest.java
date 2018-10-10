ae3fd6ea543bdbfbeb5d5b3367972cfb2e112133
bylee <bylee78@gmail.com>
2018-10-10 12:40:24 +0900

    Add a test case CommandFactory

---
+++ core/src/test/java/ship/CommandFactoryTest.java
@@ -0,0 +1,27 @@
+package ship;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import ship.util.DummyMessagePrinter;
+
+@RequiredArgsConstructor
+public class CommandFactoryTest extends AbstractTestCase {
+  @Parameters
+  public static Collection<String> data() {
+    return Arrays.asList("init", "install", "build", "test", "publish");
+  }
+
+  @Test
+  public void testCreate() {
+    final CommandFactory commandFactory = new CommandFactory(DummyMessagePrinter.getInstance());
+    for (final String command : data()) {
+      assertTrue(commandFactory.create(new String[] { command }).isPresent());
+    }
+  }
+
+}
