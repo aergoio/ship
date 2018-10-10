3c8f5c88c722814e1fff8cb06afd9894dc5dc6f2
bylee <bylee78@gmail.com>
2018-10-10 17:49:44 +0900

    Purify code

--- core/src/main/java/ship/build/web/model/QueryResult.java
+++ core/src/main/java/ship/build/web/model/QueryResult.java
@@ -1,16 +1,15 @@
 package ship.build.web.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 public class QueryResult {
 
   @Getter
   @Setter
   protected Object result;
-
-  public QueryResult() {
-  }
 
   public QueryResult(final Object result) {
     this.result = result;
