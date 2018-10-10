be61b250b206361f11d142f2e377a3770f4e9186
bylee <bylee78@gmail.com>
2018-10-10 12:34:37 +0900

    Add a test case ShipLauncher

--- core/src/test/java/ship/exec/ShipLauncherTest.java
+++ core/src/test/java/ship/exec/ShipLauncherTest.java
@@ -1,6 +1,7 @@
 package ship.exec;
 
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.spy;
@@ -8,6 +9,7 @@ import static org.powermock.api.mockito.PowerMockito.spy;
 import org.junit.Test;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import ship.AbstractTestCase;
+import ship.Command;
 import ship.util.DummyMessagePrinter;
 
 public class ShipLauncherTest extends AbstractTestCase {
@@ -18,6 +20,16 @@ public class ShipLauncherTest extends AbstractTestCase {
     final ShipLauncher shipLauncher = spy(new ShipLauncher(DummyMessagePrinter.getInstance()));
     doNothing().when(shipLauncher).exit(anyInt());
     shipLauncher.run(new String[] {});
+    verify(shipLauncher).exit(anyInt());
+  }
+
+  @Test
+  @PrepareForTest({ ShipLauncher.class, System.class })
+  public void testExecute() {
+    final ShipLauncher shipLauncher = spy(new ShipLauncher(DummyMessagePrinter.getInstance()));
+    doNothing().when(shipLauncher).exit(anyInt());
+    final Command command = mock(Command.class);
+    shipLauncher.execute(command);
     verify(shipLauncher).exit(anyInt());
   }
 }
