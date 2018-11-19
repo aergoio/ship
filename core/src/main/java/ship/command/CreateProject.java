7bf78bfb601b7f90166b42815a82f980dcf8568a
bylee <bylee78@gmail.com>
2018-11-19 11:57:51 +0900

    Fix testcase

--- core/src/main/java/ship/command/CreateProject.java
+++ core/src/main/java/ship/command/CreateProject.java
@@ -7,6 +7,7 @@ package ship.command;
 import static hera.util.FilepathUtils.getFilename;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
 import static ship.util.Messages.bind;
 
@@ -18,23 +19,27 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import ship.Command;
 import ship.ProjectFile;
+import ship.exception.CommandException;
 import ship.exception.DirectoryNotEmptyException;
+import ship.util.Messages;
 
 public class CreateProject extends AbstractCommand implements Command {
 
   protected static final String NL_0 = CreateProject.class.getName() + ".0";
   protected static final String NL_1 = CreateProject.class.getName() + ".1";
+  protected static final String NL_2 = CreateProject.class.getName() + ".2";
 
   @ToString
   class Options {
+
     @Parameter(names = {"-f", "--force"},
         description = "Force to set aergo.json up though it exists.")
     @Getter
@@ -53,22 +58,26 @@ public class CreateProject extends AbstractCommand implements Command {
     return projectFile;
   }
 
+  /**
+   * Prepare directories in project file(aergo.json).
+   *
+   * @param projectFile project configuration contents
+   */
   protected void prepare(final ProjectFile projectFile) {
     final List<Supplier<String>> fileSupplier = asList(
         projectFile::getSource,
         projectFile::getTarget);
 
     fileSupplier.stream()
-        .map(f -> f.get())
-        .forEach(path ->
-            ofNullable(path)
-                .map(File::new)
-                .map(File::getParentFile)
-                .filter(f -> !f.exists())
-                .ifPresent(File::mkdirs)
-        );
-
-    final String sourcePath = projectFile.getSource();
+        .map(Supplier::get)
+        .allMatch(path -> (null == path) ? true : of(path)
+            .map(Paths::get)
+            .map(Path::toAbsolutePath)
+            .map(Path::getParent)
+            .filter(p -> !Files.exists(p))
+            .map(Path::toFile)
+            .map(File::mkdirs)
+            .orElseThrow(() -> new CommandException(Messages.bind(NL_2, path))));
   }
 
   @Override
--- core/src/test/java/ship/command/CreateProjectTest.java
+++ core/src/test/java/ship/command/CreateProjectTest.java
@@ -6,13 +6,16 @@ package ship.command;
 
 import static java.util.Collections.EMPTY_LIST;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.nio.file.Files;
 import org.junit.Test;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -23,20 +26,23 @@ import ship.util.MessagePrinter;
 public class CreateProjectTest extends AbstractTestCase {
 
   @Test
-  @PrepareForTest(CreateProject.class)
+  @PrepareForTest({CreateProject.class, File.class})
   @SuppressWarnings("unchecked")
   public void testExecute() throws Exception {
     // Given
+    final ProjectFile projectFile = new ProjectFile();
     final WriteProjectFile writeProjectFile = mock(WriteProjectFile.class);
+    final CreateProject command = spy(new CreateProject());
+
+    whenNew(ProjectFile.class).withAnyArguments().thenReturn(projectFile);
     whenNew(WriteProjectFile.class).withAnyArguments().thenReturn(writeProjectFile);
-    when(writeProjectFile.getProject()).thenReturn(new ProjectFile());
     mockStatic(Files.class);
     when(Files.list(any())).thenReturn(EMPTY_LIST.stream());
     when(Files.newInputStream(any()))
         .thenReturn(new ByteArrayInputStream("hello, world".getBytes()));
+    doNothing().when(command).prepare(any());
 
     // When
-    final CreateProject command = new CreateProject();
     command.setPrinter(mock(MessagePrinter.class));
     command.execute();
 
